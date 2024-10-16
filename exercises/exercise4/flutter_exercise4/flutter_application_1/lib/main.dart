360724955a34e9cb9938b14ab5f4c3f53be258ba
QianWang6 <wang.qian6@northeastern.edu>
2024-10-16 10:21:34 -0700

    exercise4

---
+++ exercises/exercise4/flutter_exercise4/flutter_application_1/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'photo_hero.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HeroAnimation(),
+    );
+  }
+}
+
+class HeroAnimation extends StatelessWidget {
+  const HeroAnimation({super.key});
+
+  Widget build(BuildContext context) {
+    timeDilation = 5.0; // 1.0 means normal animation speed.
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basic Hero Animation'),
+      ),
+      body: Center(
+        child: PhotoHero(
+          photo: 'images/xiaoxin.jpg',
+          width: 300.0,
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute<void>(
+              builder: (context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text('Flippers Page'),
+                  ),
+                  body: Container(
+                    // Set background to blue to emphasize that it's a new route.
+                    color: Colors.yellow,
+                    padding: const EdgeInsets.all(16),
+                    alignment: Alignment.topLeft,
+                    child: PhotoHero(
+                      photo: 'images/xiaoxin.jpg',
+                      width: 100.0,
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                );
+              }
+            ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
